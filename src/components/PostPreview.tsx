b6165efce86efa85f7f5b2fd0316ae4fa900f888
Hussain-7 <hussain2000.rizvi@gmail.com>
2022-02-14 04:02:09 +0500

    feat(ssr) : SSR added in individual post page

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,11 +1,18 @@
-import { Box, Grid, IconButton, Paper, Typography } from "@material-ui/core";
+import {
+  ButtonBase,
+  Grid,
+  IconButton,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import React, { ReactElement } from "react";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import { Post } from "../API";
 import formatDatePosted from "../../lib/formatDate";
-
+import { useRouter } from "next/router";
 import { makeStyles } from "@material-ui/core/styles";
+import Image from "next/image";
 
 const useStyles = makeStyles({
   root: {
@@ -18,6 +25,7 @@ type Props = {
 
 const PostPreview = ({ post }: Props): ReactElement => {
   const classes = useStyles();
+  const router = useRouter();
 
   return (
     <Paper elevation={3}>
@@ -30,7 +38,14 @@ const PostPreview = ({ post }: Props): ReactElement => {
         }}
         spacing={3}
       >
-        <Grid item xs={3} sm={2} justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={3}
+          sm={2}
+          // style={{
+          //   margin: "auto",
+          // }}
+        >
           <Grid
             container
             direction="column"
@@ -62,25 +77,42 @@ const PostPreview = ({ post }: Props): ReactElement => {
             </Grid>
           </Grid>
         </Grid>
+
         <Grid item xs={9} sm={9} className={classes.root}>
-          <Grid container item>
-            <Grid container direction="column">
-              <Grid item>
-                <Typography variant="body1">
-                  Posted by <b>{post.owner} </b>{" "}
-                  {formatDatePosted(post.createdAt)}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="h2">{post.title}</Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2">
-                  {post.contents.slice(0, 200)}...
-                </Typography>
+          <ButtonBase
+            onClick={() => {
+              router.push(`post/${post.id}`);
+            }}
+          >
+            <Grid container item>
+              <Grid container direction="column" spacing={2}>
+                <Grid item>
+                  <Typography variant="body1">
+                    Posted by <b>{post.owner} </b>{" "}
+                    {formatDatePosted(post.createdAt)}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h2">{post.title}</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2">
+                    {post.contents.slice(0, 200)}...
+                  </Typography>
+                </Grid>
+                {post.image && (
+                  <Grid item>
+                    <Image
+                      src={post.image}
+                      height={540}
+                      width={980}
+                      layout="intrinsic"
+                    />
+                  </Grid>
+                )}
               </Grid>
             </Grid>
-          </Grid>
+          </ButtonBase>
         </Grid>
       </Grid>
     </Paper>
---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { GetPostQuery, ListPostsQuery, Post } from "../../API";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { withSSRContext } from "aws-amplify";
+import { getPost, listPosts } from "../../graphql/queries";
+type Props = {
+  post: Post;
+};
+
+function IndividualPosts({ post }: Props) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <div>Post:{id}</div>;
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // Call an external API endpoint to get posts.
+  const SSR = withSSRContext();
+
+  const postsQuery = (await SSR.API.graphql({
+    query: getPost,
+    variables: {
+      id: params.id,
+    },
+  })) as { data: GetPostQuery };
+
+  // By returning { props: { posts } }, the Individual Post component
+  // will receive `post` as a prop at build time
+  return {
+    props: {
+      post: postsQuery.data.getPost as Post,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 1, // In seconds
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const SSR = withSSRContext();
+
+  const response = (await SSR.API.graphql({ query: listPosts })) as {
+    data: ListPostsQuery;
+    errors: any[];
+  };
+
+  // Get the paths we want to pre-render based on posts
+  const paths = response.data.listPosts.items.map((post) => ({
+    params: { id: post.id },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: blocking } will server-render pages
+  // on-demand if the path doesn't exist.
+  return { paths, fallback: "blocking" };
+};
+
+export default IndividualPosts;
