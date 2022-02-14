6e60e8504724f00a63a8e4968ccd89835930c7c2
Hussain-7 <hussain2000.rizvi@gmail.com>
2022-02-14 05:00:57 +0500

    Comment ui completed

--- lib/formatDate.tsx
+++ lib/formatDate.tsx
@@ -3,10 +3,20 @@ export default function formatDatePosted(date: string): string {
   const current = new Date(date);
   const diff = now.getTime() - current.getTime();
   let str = " hours ago";
-  let time = (diff / 1000 / 60 / 60).toFixed(0);
+  let time = (diff / 1000 / 60).toFixed(0);
+  console.log("time:", time);
+  if (time === "0") {
+    str = " just now";
+    time = str;
+    return time;
+  }
+  time = (diff / 1000 / 60 / 60).toFixed(0);
   if (time === "0") {
     time = (diff / 1000 / 60).toFixed(0);
     str = " mins ago";
+  } else {
+    str = "hours ago";
+    time += str;
   }
   time += str;
   return time;
